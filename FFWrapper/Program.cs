baea269e130321ef99d10823a465e67d2dc7c227
Tomoaki Yasaka <34957299+tomothy83@users.noreply.github.com>
2021-09-12 10:43:55 +0900

    Add ffmpeg wrapper program.

---
+++ FFWrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FFWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
